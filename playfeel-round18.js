3c81bbb3f68b29c68a5e63d255ad00a4b1b67d16
mizuno-ikg <mizuno_ikg@hotmail.co.jp>
2026-08-28 21:51:35 +0900

    Load M10 balance controls after playfeel chain

--- playfeel-round18.js
+++ playfeel-round18.js
@@ -8,4 +8,5 @@
     @media(max-width:620px){.toast-stack{top:184px;right:8px;width:min(340px,calc(100vw - 16px))}}
   `;
   document.head.append(style);
+  const m10Logic=document.createElement('script');m10Logic.src='balance-m10-logic.js';m10Logic.onload=()=>{const m10=document.createElement('script');m10.src='balance-m10.js';document.head.append(m10)};document.head.append(m10Logic);
 })();
